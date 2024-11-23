d151c2c32e1466914bf6a6a48ec050265bc99f76
pmf586 <pmf586@alumnos.unican.es>
2024-11-23 12:44:50 +0100

    Creacion del menu de inicio, creategame y joingame

--- Assets/Scripts/MenuInicio/menu.cs
+++ Assets/Scripts/MenuInicio/menu.cs
@@ -9,11 +9,22 @@ public class NewEmptyCSharpScript : MonoBehaviour
     public GameObject Canvas1; // Canvas del menú principal
     public GameObject Canvas2; // Canvas del "Create Game"
     public GameObject Canvas3; // Canvas del "Join Game"
+    public GameObject Canvas4; // Canvas del "Join Game"
+
     public void CreateGame()
     {
         Canvas1.SetActive(false); // Desactiva el canvas del menú principal
         Canvas2.SetActive(true); // Activa el canvas del "Create Game"
         Canvas3.SetActive(false);
+        Canvas4.SetActive(false);
+    }
+
+    public void InsideGame()
+    {
+        Canvas1.SetActive(false); // Desactiva el canvas del menú principal
+        Canvas2.SetActive(false); // Activa el canvas del "Create Game"
+        Canvas3.SetActive(false);
+        Canvas4.SetActive(true);
     }
 
     public void JoinGame()
@@ -21,6 +32,7 @@ public class NewEmptyCSharpScript : MonoBehaviour
         Canvas1.SetActive(false); // Desactiva el canvas del menú principal
         Canvas2.SetActive(false); // Activa el canvas del "Create Game"
         Canvas3.SetActive(true);
+        Canvas4.SetActive(false);
     }
 
     public void VolverAlMenu()
@@ -28,6 +40,15 @@ public class NewEmptyCSharpScript : MonoBehaviour
         Canvas1.SetActive(true); // Desactiva el canvas del menú principal
         Canvas2.SetActive(false); // Activa el canvas del "Create Game"
         Canvas3.SetActive(false);
+        Canvas4.SetActive(false);
+    }
+
+    public void EsconderTodos()
+    {
+        Canvas1.SetActive(false);
+        Canvas2.SetActive(false);
+        Canvas3.SetActive(false);
+        Canvas4.SetActive(false);
     }
 
     public void Salir()
