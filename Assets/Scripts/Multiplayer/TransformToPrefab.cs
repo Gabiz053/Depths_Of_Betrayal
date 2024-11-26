08ef75fb4ae7f35461f72e808f986a9391ee0f0b
scg194 <scg194@alumnos.unican.es>
2024-11-26 04:15:59 +0100

    Creada aleatoriedad de prefab (muy guarro pero funciona)

---
+++ Assets/Scripts/Multiplayer/TransformToPrefab.cs
@@ -0,0 +1,22 @@
+using System.Linq;
+using System.Transactions;
+using UnityEngine;
+
+public class TransformToPrefab : MonoBehaviour
+{
+    public GameObject[] prefabs;  // Referencia al prefab
+
+    void Start()
+    {
+        // Verifica si el prefab está asignado
+        int randomIndex = Random.Range(0, prefabs.Count());
+
+        
+        // Instancia el prefab en la posición del objeto vacío
+        Instantiate(prefabs[randomIndex], transform.position, transform.rotation);
+        prefabs[randomIndex].SetActive(true);
+
+        // Destruye el objeto vacío después de crear el prefab
+        Destroy(gameObject);
+    }
+}
